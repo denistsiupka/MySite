bb04242023b8bdb5dc188d46d1022fd023808bc7
denistsiupka <71876670+denistsiupka@users.noreply.github.com>
2020-09-28 23:52:59 +0300

    My first site)))

---
+++ js/main.js
@@ -0,0 +1,4 @@
+
+$(".header__btn-menu").click(function () { // задаем функцию при нажатиии на элемент с классом slide-toggle
+    $(".header__menu-list").slideToggle(); // плавно скрываем, или отображаем все элементы <div>
+});
